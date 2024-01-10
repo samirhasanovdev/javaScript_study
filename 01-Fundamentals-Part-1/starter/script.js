29477f174cc35f1465258cf005656dc64653ad24
samirhasanovdev <samirhasanovdev@gmail.com>
2024-01-10 17:44:03 +0400

    mesage

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,8 +1,17 @@
-const country = 'Azerbaijan';
-const continent = 'Europe';
-let population = 10000000;
-const isIsland = false;
-const language = 'Azeri'; 
-console.log (country, continent, population, isIsland, language)
+// const country = 'Azerbaijan';
+// const continent = 'Europe';
+// let population = 10000000;
+// const isIsland = false;
+// const language = 'Azeri'; 
+// console.log (country, continent, population, isIsland, language)
 
-hi hi hi
+
+const massMark = 78;
+const heightMark =1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = (BMIMark > BMIJohn);
+console.log (BMIJohn, BMIMark, markHigherBMI);
