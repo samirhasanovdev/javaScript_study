facd294b65bcf85ddaa862d6345ecd3c23338654
samirhasanovdev <samirhasanovdev@gmail.com>
2024-01-10 16:54:30 +0400

    message

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,7 @@
+const country = 'Azerbaijan';
+const continent = 'Europe';
+let population = 10000000;
+const isIsland = false;
+const language = 'Azeri'; 
+console.log (country, continent, population, isIsland, language)
+
