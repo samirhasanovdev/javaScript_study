f15c973d1d97b563b76ddee8cf4994b9c4b374b9
samirhasanovdev <samirhasanovdev@gmail.com>
2024-01-11 18:14:03 +0400

    message

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,48 +1,63 @@
-// const country = 'Azerbaijan';
-// const continent = 'Europe';
-// let population = 10000000;
-// const isIsland = false;
-// const language = 'Azeri'; 
-// console.log (country, continent, population, isIsland, language)
+// // const country = 'Azerbaijan';
+// // const continent = 'Europe';
+// // let population = 10000000;
+// // const isIsland = false;
+// // const language = 'Azeri'; 
+// // console.log (country, continent, population, isIsland, language)
 
 
-// const massMark = 78;
-// const heightMark =1.69;
-// const massJohn = 92;
-// const heightJohn = 1.95;
+// // const massMark = 78;
+// // const heightMark =1.69;
+// // const massJohn = 92;
+// // const heightJohn = 1.95;
 
-// const BMIMark = massMark / (heightMark * heightMark);
-// const BMIJohn = massJohn / (heightJohn * heightJohn);
-// const markHigherBMI = (BMIMark > BMIJohn);
-// console.log (BMIJohn, BMIMark, markHigherBMI);
+// // const BMIMark = massMark / (heightMark * heightMark);
+// // const BMIJohn = massJohn / (heightJohn * heightJohn);
+// // const markHigherBMI = (BMIMark > BMIJohn);
+// // console.log (BMIJohn, BMIMark, markHigherBMI);
 
-// const firstName = 'Samir';
-// const birthYear = 1994;
-// const job = 'ArmyMan';
-// const year = 2024;
+// // const firstName = 'Samir';
+// // const birthYear = 1994;
+// // const job = 'ArmyMan';
+// // const year = 2024;
 
-// const samirNew = `I'm ${firstName}, a ${year-birthYear} ${job}`
-// console.log ('')
+// // const samirNew = `I'm ${firstName}, a ${year-birthYear} ${job}`
+// // console.log ('')
 
 
-const age = 25;
+// const age = 25;
 
-if (age >=18) {
-    console.log ('Samir can drive')
-}
-else {
-    const yearsLeft = 18 - age;
-    console.log (`You should wait for another ${yearsLeft} years`)
-     }
+// if (age >=18) {
+//     console.log ('Samir can drive')
+// }
+// else {
+//     const yearsLeft = 18 - age;
+//     console.log (`You should wait for another ${yearsLeft} years`)
+//      }
     
 
-const birthYear = 2024;
-let century;
-if (birthYear >= 2000) {
-    century = 21;
+// const birthYear = 2024;
+// let century;
+// if (birthYear >= 2000) {
+//     century = 21;
+// }
+// else {
+//     century = 20;
+// }
+
+// console.log (century)
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+// console.log(BMIMark, BMIJohn);
+
+if (BMIMark > BMIJohn) {
+    console.log (`Mark's BMI ${BMIMark} is higher than John's! ${BMIJohn}`)
 }
 else {
-    century = 20;
+    console.log (`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark} !`)
 }
-
-console.log (century)
