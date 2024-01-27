22d8c78389344a6dd167e1e03040e2b2e1962c85
samirhasanovdev <samirhasanovdev@gmail.com>
2024-01-27 13:42:24 +0400

    message

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -108,5 +108,6 @@
 
 
 
-const tip 
-const bill
+const bill = 275;
+const tip = bill >= 50 && bill <= 300 ?  (bill * 15) / 100 : (bill * 20) / 100;
+console.log (`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
