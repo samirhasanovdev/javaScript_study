9b94b220f58c629074ab5ed6bfd788938c873205
samirhasanovdev <samirhasanovdev@gmail.com>
2024-02-07 16:10:35 +0400

    message

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,2 +1,16 @@
 'use script'
-sadsdsdadsfsdfsdfsfsdf
+
+
+const cutPieces = function (fruit) {
+    return fruit * 4;
+}
+
+const juiceMaker = function (apples, oranges) {
+    const applesPieces = cutPieces (apples);
+    const orangePieces = cutPieces (oranges);
+
+    const juice = `I have ${applesPieces} and ${orangePieces}`;
+    return juice;
+}
+
+console.log (juiceMaker(10, 20))
