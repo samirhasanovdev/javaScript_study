081ae205257deaa5a4254ecc4077b7218f912e82
samirhasanovdev <samirhasanovdev@gmail.com>
2024-02-10 10:41:12 +0400

    message

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,2 +1,21 @@
 'use script'
-sadsdsdadsfsdfsdfsfsdf
+
+
+const calcAverage = (a, b, c)  => (a + b + c) / 3;
+
+
+const checkWinner = function (avgDolhins, avgKoalas) {
+    if (avgDolhins >= 2 * avgKoalas)
+    return `Dolphins win (${avgDolhins} vs ${avgKoalas})`;
+    else if (avgKoalas >= 2 * avgDolhins)
+    return `Koalas win (${avgKoalas} vs ${avgDolhins})`
+    else return 'No team wins...'
+};
+    
+    const scoreDolphins = calcAverage (44, 23, 71);
+    const scoreKoalas = calcAverage(65, 54, 49);
+    const scoreDolphins2 = calcAverage (85, 54, 41);
+    const scoreKoalas2 = calcAverage(23, 34, 27);
+    
+console.log (checkWinner (scoreDolphins, scoreKoalas));
+console.log (checkWinner (scoreDolphins2, scoreKoalas2));
