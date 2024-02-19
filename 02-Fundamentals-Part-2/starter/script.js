63ee5f5a4ef9eb8c98357ccb7637890aa60a596b
samirhasanovdev <samirhasanovdev@gmail.com>
2024-02-19 19:05:30 +0400

    message

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -20,9 +20,20 @@
 // console.log (checkWinner (scoreDolphins, scoreKoalas));
 // console.log (checkWinner (scoreDolphins2, scoreKoalas2));
 
-const calcTip = function (bill) {
-return bill >= 50 && bill <= 300 ? bill * 0.15 : bill *0.2
-}
-const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-const total = [bills[0] + tips[0],bills[1] + tips[1], bills[2] + tips[2]];
+// const calcTip = function (bill) {
+// return bill >= 50 && bill <= 300 ? bill * 0.15 : bill *0.2
+// }
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// const total = [bills[0] + tips[0],bills[1] + tips[1], bills[2] + tips[2]];
+
+const samir =  {
+    firsstName: 'Samir',
+    lastName: 'Hasanov',
+    age: 2024 - 1994,
+    job: 'dev',
+    friends: ['Peter', 'Steven', 'Lara']
+};
+
+
+console.log (samir['firsstName'], 'has' , samir['friends'].length, 'friends, and his best friend is called', samir['friends'][0])
